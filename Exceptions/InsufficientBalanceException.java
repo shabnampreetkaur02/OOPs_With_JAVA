d4fc4c99e4a683db1408f733677988c4a47d0676
Shabnampreet <shabnampreetk2@gmail.com>
2026-03-11 19:48:03 +0530

    Delete Exceptions directory

--- Exceptions/InsufficientBalanceException.java
+++
@@ -1,9 +0,0 @@
-package com.Bank.Exceptions;
-
-public class InsufficientBalanceException extends Exception {
-
-    public InsufficientBalanceException(String message){
-        super(message);
-    }
-
-}
